9066271980efaa1a561a912381604cb7921586c1
alannascimento87 <alan.nicolascosta@gmail.com>
2021-08-12 14:19:21 -0400

    Versão com botão para seleção da cor de fundo do cartão

--- app/src/main/java/br/com/dio/businesscard/ui/AddBusinessCardActivity.kt
+++ app/src/main/java/br/com/dio/businesscard/ui/AddBusinessCardActivity.kt
@@ -2,14 +2,20 @@ package br.com.dio.businesscard.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.viewModels
 import br.com.dio.businesscard.App
 import br.com.dio.businesscard.R
 import br.com.dio.businesscard.data.BusinessCard
 import br.com.dio.businesscard.databinding.ActivityAddBusinessCardBinding
+import com.github.dhaval2404.colorpicker.MaterialColorPickerDialog
+import com.github.dhaval2404.colorpicker.model.ColorShape
+import com.github.dhaval2404.colorpicker.model.ColorSwatch
 
 class AddBusinessCardActivity : AppCompatActivity() {
+
+    private var mDefaultColor: String = ""
 
     private val binding by lazy {ActivityAddBusinessCardBinding.inflate(layoutInflater)}
 
@@ -30,13 +36,27 @@ class AddBusinessCardActivity : AppCompatActivity() {
             finish()
         }
 
+        binding.btnColor.setOnClickListener { _ ->
+            MaterialColorPickerDialog
+                .Builder(this)        					// Pass Activity Instance
+                .setTitle("Pick Theme")           		// Default "Choose Color"
+                .setColorShape(ColorShape.SQAURE)   	// Default ColorShape.CIRCLE
+                .setColorSwatch(ColorSwatch._300)   	// Default ColorSwatch._500
+                .setDefaultColor(mDefaultColor) 		// Pass Default Color
+                .setColorListener { color, colorHex ->
+                    mDefaultColor = colorHex
+
+                }
+                .show()
+        }
+
         binding.btnConfirmar.setOnClickListener{
             val businessCard = BusinessCard(
                 nome = binding.tilNome.editText?.text.toString(),
                 empresa = binding.tilEmpresa.editText?.text.toString(),
                 telefone = binding.tilTelefone.editText?.text.toString(),
                 email = binding.tilEmail.editText?.text.toString(),
-                fundoPersonalizado = binding.tilCor.editText?.text.toString()
+                fundoPersonalizado = mDefaultColor.uppercase() ,
             )
             mainViewModel.insert(businessCard)
             Toast.makeText(this, R.string.label_show_sucess,Toast.LENGTH_SHORT).show()
